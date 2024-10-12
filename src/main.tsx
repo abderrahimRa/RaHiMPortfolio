27409dfb6110ee7456ffecc86e905338ff89bd9a
abderrahimRa <khobiziziabdou@gmail.com>
2024-10-12 02:22:34 +0100

    Update configuration for GitHub Pages deployment

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import Skills from "./routes/Skills.tsx";
 import Experience from "./routes/Experience.tsx";
 import Contact from "./routes/Contact.tsx";
-const router = createBrowserRouter([
+
+const router = createHashRouter([
   { path: "/", element: <App /> },
   { path: "/Skills", element: <Skills /> },
   { path: "/Experience", element: <Experience /> },
   { path: "/Contact", element: <Contact /> },
 ]);
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
